656d1b3db10565bb057614627fcb5aeb01d7a648
masquee <okayanz@outlook.com>
2022-07-04 23:51:15 +0800

    refactor

--- 04/src/monkey/main.go
+++ 04/src/monkey/main.go
@@ -8,12 +8,12 @@ import (
 )
 
 func main() {
-	user, err := user.Current()
+	u, err := user.Current()
 	if err != nil {
 		panic(err)
 	}
 	fmt.Printf("Hello %s! This is the Monkey programming language!\n",
-		user.Username)
+		u.Username)
 	fmt.Printf("Feel free to type in commands\n")
 	repl.Start(os.Stdin, os.Stdout)
 }
